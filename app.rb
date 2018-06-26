f207e386ee571c053fc51f2b90addf830dc9a30a
lexiehuynh <lexiehuynh88@gmail.com>
2018-06-26 14:04:46 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/hello' do
+    "Hello, World!"
+  end 
+  get '/name' do
+    "My name is Lexie!"
+  end 
+  get '/hometown' do
+    "My hometown is Suwanee, Georgia"
+  end 
+  get '/favorite-song' do
+    "My favorite song is bleach!"
+  end 
 end
