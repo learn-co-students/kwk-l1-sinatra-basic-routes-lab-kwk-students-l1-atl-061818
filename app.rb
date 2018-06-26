297468cd8d3084c62b454bea32e209da7278430e
vidyagopal <vidya2732@gmail.com>
2018-06-26 14:06:08 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get "/" do
+    "Hello, World!"
+  end
+  get "/name" do
+    "My name is Vidya"
+  end
+  get "/hometown" do
+    "My hometown is Fort Collins, Colorado"
+  end
+  get "/favorite-song" do
+    "My favorite song is Love Story, by Taylor Swift"
+  end
 end
